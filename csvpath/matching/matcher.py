37d188f1c62913bf3549e3b6fea2121b8d539a42
David Kershaw <dk107dk@hotmail.com>
2024-09-03 14:14:06 -0400

    pylinting matcher

--- csvpath/matching/matcher.py
+++ csvpath/matching/matcher.py
@@ -1,16 +1,21 @@
+""" Matching is the core of CsvPath. """
+
 from typing import Any, List
-from ..util.parser_utility import ParserUtility
-from .productions import *
-from .functions.function_factory import FunctionFactory
+from .productions import Equality
 from .functions.function import Function
 from .util.expression_encoder import ExpressionEncoder
 from .util.exceptions import MatchException
-from ..util.exceptions import VariableException
-from ..util.exceptions import ConfigurationException
 from . import LarkParser, LarkTransformer
 
 
-class Matcher:
+class Matcher:  # pylint: disable=R0902
+    """Matcher implements the match component rules processing that
+    is applied to files line-by-line. matchers are created at the
+    beginning of a run and are reset and reused for every line.
+    """
+
+    # re: R0902: no obvious improvements
+
     def __init__(self, *, csvpath=None, data=None, line=None, headers=None):
         if not headers:
             # this could be a dry-run or unit testing
@@ -22,7 +27,6 @@ class Matcher:
         self._line = line
         self.expressions = []
         self.if_all_match = []
-        self.current_expression = None
         self.skip = False
         if data is not None:
             self.parser = LarkParser()
@@ -45,29 +49,32 @@ class Matcher:
         """
 
     @property
-    def line(self) -> List[List[Any]]:
+    def line(self) -> List[List[Any]]:  # pylint: disable=C0116
         return self._line
 
     @line.setter
     def line(self, line: List[List[Any]]) -> None:
         self._line = line
 
-    def to_json(self, e) -> str:
+    def to_json(self, e) -> str:  # pylint: disable=C0116
         return ExpressionEncoder().to_json(e)
 
-    def dump_all_expressions_to_json(self) -> str:
+    def dump_all_expressions_to_json(self) -> str:  # pylint: disable=C0116
         return ExpressionEncoder().valued_list_to_json(self.expressions)
 
-    def reset(self):
+    def reset(self):  # pylint: disable=C0116
         for expression in self.expressions:
             expression[1] = None
             expression[0].reset()
-        self.current_expression = None
 
     def header_index(self, name: str) -> int:
+        """returns the index of a header name in the current headers. remember that
+        a header_reset() can change the indexes mid file."""
         return self.csvpath.header_index(name)
 
     def header_name(self, i: int) -> str:
+        """returns the name of a header given an index into the current headers.
+        remember that a header_reset() can change the indexes mid file."""
         if not self.csvpath.headers:
             return None
         if i < 0 or i >= len(self.csvpath.headers):
@@ -75,6 +82,8 @@ class Matcher:
         return self.csvpath.headers[i]
 
     def header_value(self, name: str) -> Any:
+        """returns the value of a header name in the current line for the current
+        headers. remember that a header_reset() can change the indexes mid file."""
         n = self.header_index(name)
         ret = None
         if n is None:
@@ -84,7 +93,7 @@ class Matcher:
         return ret
 
     def _do_lasts(self) -> None:
-        for i, et in enumerate(self.expressions):
+        for et in self.expressions:
             e = et[0]
             self._find_and_actvate_lasts(e)
 
@@ -106,6 +115,8 @@ class Matcher:
                 cs += c.children
 
     def matches(self, *, syntax_only=False) -> bool:
+        """this is the main work of the Matcher. we enumerate the self.expressions.
+        if all evaluate to True in an AND operation we return True."""
         #
         # is this a blank last line? if so, we just want to activate any/all
         # last() in the csvpath.
@@ -116,8 +127,7 @@ class Matcher:
 
         ret = True
         failed = False
-        self.current_expression = None
-        for i, et in enumerate(self.expressions):
+        for et in self.expressions:
             if self.csvpath and self.csvpath.stopped:
                 #
                 # stopped is like a system halt. this csvpath is calling it
@@ -126,7 +136,7 @@ class Matcher:
                 # because the function already let the CsvPath know to stop.
                 #
                 return False
-            elif self.skip is True:
+            if self.skip is True:
                 #
                 # skip is like the continue statement in a python loop
                 # we're not only not matching, we don't want any side effects
@@ -135,7 +145,6 @@ class Matcher:
                 #
                 self.skip = False
                 return False
-            self.current_expression = et[0]
             if et[1] is True:
                 ret = True
             elif et[1] is False:
@@ -165,11 +174,11 @@ class Matcher:
         #
         return ret
 
-    def check_valid(self) -> None:
+    def check_valid(self) -> None:  # pylint: disable=C0116
         for _ in self.expressions:
             _[0].check_valid()
 
-    def do_set_if_all_match(self) -> None:
+    def do_set_if_all_match(self) -> None:  # pylint: disable=C0116
         for _ in self.if_all_match:
             name = _[0]
             value = _[1]
@@ -178,25 +187,30 @@ class Matcher:
         self.if_all_match = []
 
     def set_if_all_match(self, name: str, value: Any, tracking=None) -> None:
+        """registers a variable set to happen only after the line's consideration
+        is complete and it is found to match. this is used for setting variables
+        when they have 'onmatch'; however, more recently we handle onmatch in
+        another way. you can see that in Matchable."""
         self.if_all_match.append((name, value, tracking))
 
     def get_variable(self, name: str, *, tracking=None, set_if_none=None) -> Any:
+        """see CsvPath.get_variable"""
         if self.csvpath is None:
             return None
-        else:
-            return self.csvpath.get_variable(
-                name, tracking=tracking, set_if_none=set_if_none
-            )
+        return self.csvpath.get_variable(
+            name, tracking=tracking, set_if_none=set_if_none
+        )
 
     def set_variable(self, name: str, *, value: Any, tracking=None) -> None:
+        """see CsvPath.set_variable"""
         return self.csvpath.set_variable(name, value=value, tracking=tracking)
 
-    def last_header_index(self) -> int:
+    def last_header_index(self) -> int:  # pylint: disable=C0116
         if self.line and len(self.line) > 0:
             return len(self.line) - 1
         return None
 
-    def last_header_name(self) -> str:
+    def last_header_name(self) -> str:  # pylint: disable=C0116
         if self.csvpath.headers and len(self.csvpath.headers) > 0:
             return self.csvpath.headers[self.last_header_index()]
         return None
