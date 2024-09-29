32682371c6a5c76736b95434ee7d50f6aa24347b
David Kershaw <dk107dk@hotmail.com>
2024-09-29 12:16:22 -0400

    comment mode test

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,5 +1,6 @@
 import unittest
-from csvpath.csvpath import CsvPath
+from csvpath import CsvPath
+from csvpath import CsvPaths
 
 PATH = "tests/test_resources/test.csv"
 
@@ -31,11 +32,46 @@ class TestComments(unittest.TestCase):
         assert path.collect_when_not_matched is True
         assert path.has_default_printer is False
 
+    def test_comment_settings_affecting_multiple_paths(self):
+        paths = CsvPaths()
+        """~ 3 paths:
+                - AND no matches all returned
+                - OR all match all returned
+                - AND no matches none returned
+                - OR all match none returned
+        ~"""
+        paths.file_manager.add_named_file(
+            name="test", path="tests/test_resources/test.csv"
+        )
+        settings = {}
+        settings["settings"] = [
+            """~ logic-mode:AND match-mode:no-matches print-mode:default-on ~ $[1][ yes() no() print("Hi $.csvpath.line_number")]""",
+            """~ logic-mode:OR match-mode:matches print-mode:default-on  ~ $[2][ yes() no() print("Hi $.csvpath.line_number")]""",
+            """~ logic-mode:AND match-mode:matches print-mode:default-off  ~ $[3][ yes() no() print("Hi $.csvpath.line_number")]""",
+            """~ logic-mode:OR match-mode:no-matches print-mode:default-off ~ $[4][ yes() no() print("Hi $.csvpath.line_number")]""",
+        ]
+        paths.paths_manager.set_named_paths(settings)
+        paths.collect_paths(filename="test", pathsname="settings")
+        results = paths.results_manager.get_named_results("settings")
+        assert len(results) == 4
+        assert len(results[0]) == 1
+        # results has 1 line that was printed by print()
+        # results[0].csvpath handed that 1 line off to two printers: StdOut and the Result
+        assert results[0].lines_printed < len(results[0].csvpath.printers)
+        assert len(results[1]) == 1
+        assert results[1].lines_printed < len(results[1].csvpath.printers)
+        assert len(results[2]) == 0
+        # the print mode removes StdOut from results[2].csvpath leaving 1 printer, Result
+        assert results[2].lines_printed == len(results[2].csvpath.printers)
+        assert len(results[3]) == 0
+        assert results[3].lines_printed == len(results[3].csvpath.printers)
+
     def test_comments_below(self):
         path = CsvPath()
         path.parse(
             f"""
-            ~ this is my path. logic: AND collect: no-matches : ~
+            ~ checking collection of metadata,
+              not the actual modes. logic: AND collect: no-matches : ~
             ${PATH}[1*]
             [
                 ~ this path is simple and so are its comments ~
@@ -89,22 +125,11 @@ class TestComments(unittest.TestCase):
                 ~
                     [$#@"()!%^&*`@-/_=+{}#|  \\;:',.<>?()/"$[
                 ~
-                push.onmatch("cnt", count_lines())
-                count.nocontrib() == 3 -> advance(2)
+                push("d", line_number())
             ]"""
         )
         path.fast_forward()
-        # action    physical line     data line             matches           scans
-        # -----------------------------------------------------------------------------------
-        # skip  0 - line_number = 0 - data_line_count = 1 - match count = 0 - scan count = 0
-        # match 1 - line_number = 1 - data_line_count = 2 - match count = 1 - scan count = 1
-        # match 2 - line_number = 2 - data_line_count = 3 - match count = 2 - scan count = 2
-        # match 3 - line_number = 3 - data_line_count = 4 - match count = 3 -> advance(2)  3
-        # skip    - line_number = 4 - data_line_count = 5 - match count = 3 - scan count = 4
-        # skip    - line_number = 5 - data_line_count = 6 - match count = 3 - scan count = 5
-        # match 4 - line_number = 6 - data_line_count = 7 - match count = 4 - scan count = 6
-        # ...
-        assert path.variables["cnt"] == [2, 3, 4, 7, 8, 9]
+        assert len(path.variables["d"]) == 8
 
     def test_comments_back_to_back_and_empty(self):
         path = CsvPath()
