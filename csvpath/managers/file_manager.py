60cc371bbeb63101b286b1a768f709a1f65f4c71
David Kershaw <dk107dk@hotmail.com>
2024-10-29 18:57:30 -0400

    created readers for xslx, csv

--- csvpath/managers/file_manager.py
+++ csvpath/managers/file_manager.py
@@ -3,13 +3,66 @@ import os
 import json
 import csv
 import hashlib
+from abc import ABC, abstractmethod
+
+
+import pylightxl as xl
+
 from json import JSONDecodeError
 from typing import Dict, List, Tuple
-from abc import ABC, abstractmethod
 from ..util.line_counter import LineCounter
 from ..util.line_monitor import LineMonitor
 from ..util.error import ErrorHandler
 from ..util.cache import Cache
+
+
+class CsvDataFileReader(ABC):
+    def __new__(cls, path: str, *, delimiter=None, quotechar=None):
+        print(f"CsvDataFileReader.__new__: cls: {cls}")
+        if cls == CsvDataFileReader:
+            if path.endswith("xlsx"):
+                return XlsxDataReader(path)
+            else:
+                return CsvDataReader(path)
+        else:
+            instance = super().__new__(cls)
+            return instance
+
+    @abstractmethod
+    def next(self) -> list[str]:
+        pass
+
+
+class CsvDataReader(CsvDataFileReader):
+    def __init__(self, path: str, *, delimiter=None, quotechar=None) -> None:
+        self._path = path
+        self._delimiter = delimiter if delimiter is not None else ","
+        self._quotechar = quotechar if quotechar is not None else '"'
+
+    def next(self) -> list[str]:
+        with open(self._path, "r", encoding="utf-8") as file:
+            reader = csv.reader(
+                file, delimiter=self._delimiter, quotechar=self._quotechar
+            )
+            for line in reader:
+                yield line
+
+
+class XlsxDataReader(CsvDataFileReader):
+    def __init__(self, path: str, *, delimiter=None, quotechar=None) -> None:
+        self._path = path
+        self._sheet = None
+        if path.find("#") > -1:
+            self._path = path[0 : path.find("#")]
+            self._sheet = path[path.find("#") + 1]
+
+    def next(self) -> list[str]:
+        db = xl.readxl(fn=self._path)
+        if not self._sheet:
+            self._sheet = db.ws_names[0]
+
+        for row in db.ws(ws=self._sheet).rows:
+            yield [f"{datum}" for datum in row]
 
 
 class CsvPathsFileManager(ABC):
@@ -53,7 +106,7 @@ class CsvPathsFileManager(ABC):
 
 
 class FileManager(CsvPathsFileManager):  # pylint: disable=C0115
-    def __init__(self, *, named_files: Dict[str, str] = None, csvpaths):
+    def __init__(self, *, named_files: Dict[str, str] = None, csvpaths=None):
         if named_files is None:
             named_files = {}
         self.named_files: Dict[str, str] = named_files
@@ -141,6 +194,14 @@ class FileManager(CsvPathsFileManager):  # pylint: disable=C0115
         if name not in self.named_files:
             return None
         return self.named_files[name]
+
+    def get_named_file_reader(self, name: str) -> CsvDataFileReader:
+        path = self.get_named_file(name)
+        return FileManager.get_reader(path)
+
+    @classmethod
+    def get_reader(cls, path: str, delimiter=None, quotechar=None) -> CsvDataFileReader:
+        return CsvDataFileReader(path, delimiter=delimiter, quotechar=quotechar)
 
     def remove_named_file(self, name: str) -> None:
         if name in self.named_files:
