b4a40e115823c2fb7084f2f3f67bd616205114a8
David Kershaw <dk107dk@hotmail.com>
2025-02-21 14:03:05 -0500

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,46 +13,20 @@ def clear_files(request):
     files = config.inputs_files_path
     paths = config.inputs_csvpaths_path
     cache = config.cache_dir_path
-    local_paths = []
-    sftp_paths = []
-    s3_paths = []
+
+    print("cleaning up local ahead of the run")
     for p in [archive, paths, files, cache]:
         if p is None:
             continue
         p = p.strip()
-        if p.startswith("sftp://"):
-            sftp_paths.append(p)
-        elif p.startswith("s3://"):
-            s3_paths.append(p)
-        else:
-            local_paths.append(p)
-
-    print("cleaning up local ahead of the run")
-    for p in local_paths:
-        nos = Nos(p)
-        print(f"checking {p} for old test run files")
-        # if os.path.exists(p):
-        if nos.exists():
-            print(f"deleting from {p}")
-            nos.remove()
-        #   print(f"deleting from {p}")
-        #   shutil.rmtree(p)
-
-    print("cleaning up local ahead of the run")
-    for p in sftp_paths:
         nos = Nos(p)
         print(f"checking {p} for old test run files")
         if nos.exists():
             print(f"deleting from {p}")
-            nos.remove()
-
-    print("cleaning up local ahead of the run")
-    for p in s3_paths:
-        nos = Nos(p)
-        print(f"checking {p} for old test run files")
-        if nos.exists():
-            print(f"deleting from {p}")
-            nos.remove()
+            try:
+                nos.remove()
+            except Exception as e:
+                print(f"Error in cleaning: {type(e)}: {e}")
 
     print("cleaning complete.")
     print(
