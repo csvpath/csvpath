ecaac44a525c62e1f7709c1261624c3693d08250
David Kershaw <dk107dk@hotmail.com>
2025-07-02 17:01:05 -0400

    tweak

--- assets/gocd/windows/local.bat
+++ assets/gocd/windows/local.bat
@@ -2,6 +2,11 @@ set CSVPATH_CONFIG_PATH=assets\config\jenkins-windows-local.ini
 echo %CSVPATH_CONFIG_PATH%
 cmd.exe /C c:\\dev\exports.bat
 :: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env remove --all
+
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env remove --all
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env use python
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe install --only-binary=cryptography
+
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe install
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pytest
 
