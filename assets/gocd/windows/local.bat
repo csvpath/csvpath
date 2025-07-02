37d706bbcdc9fcbc33337ae097bf93fedc9d400e
David Kershaw <dk107dk@hotmail.com>
2025-07-02 17:19:21 -0400

    tweak

--- assets/gocd/windows/local.bat
+++ assets/gocd/windows/local.bat
@@ -1,7 +1,8 @@
 set CSVPATH_CONFIG_PATH=assets\config\jenkins-windows-local.ini
 echo %CSVPATH_CONFIG_PATH%
 cmd.exe /C c:\\dev\exports.bat
-:: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env remove --all
+
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env remove --all
 
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip uninstall -y cryptography
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --prefer-binary cryptography
