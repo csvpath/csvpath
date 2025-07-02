0d06be9f30e8a308f0a6c676d5451629436a9a71
David Kershaw <dk107dk@hotmail.com>
2025-07-02 17:10:21 -0400

    tweak

--- assets/gocd/windows/local.bat
+++ assets/gocd/windows/local.bat
@@ -3,7 +3,9 @@ echo %CSVPATH_CONFIG_PATH%
 cmd.exe /C c:\\dev\exports.bat
 :: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe env remove --all
 
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip uninstall cryptography
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --prefer-binary cryptography
+
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe install
 cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pytest
 
