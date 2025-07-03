872238892738082e1f8bf6883fee5dd090f06470
David Kershaw <dk107dk@hotmail.com>
2025-07-02 20:43:20 -0400

    tweak

--- assets/gocd/windows/local.bat
+++ assets/gocd/windows/local.bat
@@ -14,7 +14,8 @@ cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip uninstall -y cryptogra
 :: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --no-binary cryptography cryptography
 :: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --prefer-binary cryptography
 
-cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --no-binary --force-reinstall --platform win_amd64 cryptography cryptography
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --force-reinstall --no-binary paramiko paramiko
+cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --force-reinstall --no-binary cryptography cryptography
 
 :: cmd.exe /C c:\\Users\python\.local\bin\poetry.exe run pip install --force-reinstall --no-deps cryptography --platform win_amd64 --only-binary=cryptography
 
