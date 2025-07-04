89bd8dce62d074c11505e3fa84a0a7d25662c384
David Kershaw <dk107dk@hotmail.com>
2025-07-03 20:17:06 -0400

    tweak

--- assets/gocd/windows/local.bat
+++ assets/gocd/windows/local.bat
@@ -4,16 +4,41 @@
 ::
 set CSVPATH_CONFIG_PATH=assets\config\jenkins-windows-local.ini
 
+
+set AWS_ACCESS_KEY_ID=""
+set AWS_SECRET_ACCESS_KEY=""
+set SFTP_USER=""
+set SFTP_PASSWORD=""
+set MAILBOX_USER=""
+set MAILBOX_PASSWORD=""
+set TINPENNY_USER=""
+set TINPENNY_PASSWORD=""
+set SFTPPLUS_ADMIN_USERNAME=""
+set SFTPPLUS_ADMIN_PASSWORD=""
+set SFTPPLUS_SERVER=""
+set SFTPPLUS_PORT=""
+set GCS_CREDENTIALS_PATH=""
+set TEST_VAR=""
+set AZURE_STORAGE_ACCOUNT_NAME=""
+set AZURE_STORAGE_ACCOUNT_KEY=""
+set AZURE_STORAGE_CONNECTION_STRING=""
+set OTEL_EXPORTER_OTLP_PROTOCOL=""
+set OTEL_EXPORTER_OTLP_ENDPOINT=""
+set OTEL_EXPORTER_OTLP_HEADERS=""
+set OTEL_SERVICE_NAME=""
+set OTEL_RESOURCE_ATTRIBUTES=""
+
+
 cmd.exe /C call c:\dev\exports.bat
 
 ::cmd.exe /C set > gocd_env.txt
-echo "\nGCS path: "
+echo "\n\nGCS path: "
 echo %GCS_CREDENTIALS_PATH%
 echo "CsvPath config path: "
 echo %CSVPATH_CONFIG_PATH%
-echo "Login account path: "
-echo %PATH%
-echo ""
+::echo "Login account path: "
+::echo %PATH%
+echo "\n"
 ::
 :: these four lines DO NOT work fine. that was a mistake.
 ::
