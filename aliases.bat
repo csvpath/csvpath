fbf0c5d8d5dbcdfb25e2ebe27a2c50b595a5b43a
David Kershaw <dk107dk@hotmail.com>
2025-07-04 14:57:09 -0400

    aliases

---
+++ aliases.bat
@@ -0,0 +1,9 @@
+doskey pytest=poetry run pytest
+doskey set_local=set CSVPATH_CONFIG_PATH=assets/config/config.ini
+doskey set_sftp=set CSVPATH_CONFIG_PATH=assets/config/jenkins-local-sftp.ini
+doskey set_s3=set CSVPATH_CONFIG_PATH=assets/config/jenkins-local-s3.ini
+doskey set_azure=set CSVPATH_CONFIG_PATH=assets/config/jenkins-local-azure.ini
+doskey set_gcs=set CSVPATH_CONFIG_PATH=assets/config/jenkins-local-gcs.ini
+doskey svk=pytest -s -v -k
+
+
