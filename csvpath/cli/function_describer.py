92ce46b3319170e68b4ae7a61e8828af46fb11ae
David Kershaw <dk107dk@hotmail.com>
2025-12-18 14:53:38 -0500

    finished markdown funcs

--- csvpath/cli/function_describer.py
+++ csvpath/cli/function_describer.py
@@ -16,7 +16,9 @@ class FunctionDescriber:
         return FunctionDescriber.CONST
 
     @classmethod
-    def describe(cls, function: Function, *, markdown=False, const=None) -> None:
+    def describe(
+        cls, function: Function, *, markdown=False, const=None, links: dict = None
+    ) -> None:
         print("")
         if not function.args:
             #
@@ -37,11 +39,14 @@ class FunctionDescriber:
             for i, _ in enumerate(function.description):
                 print(_)
                 print("")
-        cls.print_tables(function, markdown=markdown)
+        cls.print_tables(function, markdown=markdown, links=links)
         print("")
+        print(
+            "[[Back to index](https://github.com/csvpath/csvpath/blob/main/docs/func_gen/index.md)]"
+        )
 
     @classmethod
-    def sigs(cls, function, *, markdown=False):
+    def sigs(cls, function, *, markdown=False, links: dict = None):
         sigs = []
         args = function.args
         PIPE = " ǁ " if markdown else "|"
@@ -59,6 +64,12 @@ class FunctionDescriber:
                     t += f"{_.name}: "
                 for j, act in enumerate(_.actuals):
                     an = cls._actual_name(act)
+                    #
+                    # add link if exact match and we have links
+                    #
+                    if markdown and links and an in links:
+                        an = f"$${{\\color{{green}}{an}}}$$"
+
                     if an == "":
                         an = "''"
                     t += f"{cls.const().SIDEBAR_COLOR}{cls.const().ITALIC}{an}{cls.const().REVERT}"
@@ -79,7 +90,7 @@ class FunctionDescriber:
         return sigs
 
     @classmethod
-    def funcs(cls, function, *, markdown=False):
+    def funcs(cls, function, *, markdown=False, links=None):
         sigs = []
         args = function.args
         PIPE = " ǁ " if markdown else "|"
@@ -96,6 +107,9 @@ class FunctionDescriber:
                     an = cls._actual_name(act)
                     if an == "":
                         an = "''"
+                    if markdown and links and an in links:
+                        an = f"[{an}]({links[an]})"
+
                     t += f"{cls.const().SIDEBAR_COLOR}{cls.const().ITALIC}{an}{cls.const().REVERT}"
                     if j < len(_.types) - 1:
                         t += PIPE
@@ -114,7 +128,7 @@ class FunctionDescriber:
         return sigs
 
     @classmethod
-    def focus_stmt(cls, function, *, markdown=False):
+    def focus_stmt(cls, function, *, markdown=False, links=None):
         stmts = []
         vp = isinstance(function, ValueProducer)
         md = isinstance(function, MatchDecider)
@@ -131,7 +145,7 @@ class FunctionDescriber:
         return stmts
 
     @classmethod
-    def type_stmt(cls, function, *, markdown=False):
+    def type_stmt(cls, function, *, markdown=False, links: dict = None):
         stmts = []
         if isinstance(function, Type):
             t = f"{function.name[0].upper()}{function.name[1:]}"
@@ -139,20 +153,20 @@ class FunctionDescriber:
         return stmts
 
     @classmethod
-    def aliases_stmt(cls, function, *, markdown=False):
+    def aliases_stmt(cls, function, *, markdown=False, links=None):
         stmts = []
         if len(function.aliases) > 0:
             stmts.append(", ".join(function.aliases))
         return stmts
 
     @classmethod
-    def print_tables(cls, function, *, markdown=False):
+    def print_tables(cls, function, *, markdown=False, links: dict = None):
         #
         # data sigs
         #
         headers = ["Data signatures"]
         rows = []
-        sigs = cls.sigs(function, markdown=markdown)
+        sigs = cls.sigs(function, markdown=markdown, links=links)
         for v in sigs:
             v = str(v)
             rows.append([v])
@@ -170,7 +184,7 @@ class FunctionDescriber:
         #
         headers = ["Call signatures"]
         rows = []
-        sigs = cls.funcs(function, markdown=markdown)
+        sigs = cls.funcs(function, markdown=markdown, links=links)
         for v in sigs:
             v = str(v)
             rows.append([v])
@@ -188,15 +202,15 @@ class FunctionDescriber:
         #
         rows = []
         headers = ["Purpose", "Value"]
-        stmts = cls.focus_stmt(function)
+        stmts = cls.focus_stmt(function, links=links)
         for v in stmts:
             v = str(v)
             rows.append(["Main focus", v])
-        stmts = cls.type_stmt(function)
+        stmts = cls.type_stmt(function, links=links)
         for v in stmts:
             v = str(v)
             rows.append(["Type", v])
-        stmts = cls.aliases_stmt(function)
+        stmts = cls.aliases_stmt(function, links=links)
         for v in stmts:
             v = str(v)
             rows.append(["Aliases", v])
@@ -220,6 +234,10 @@ class FunctionDescriber:
             for s in stmts
         ]
         if len(stmts) > 0:
+            for i, s in enumerate(stmts):
+                if markdown and links and s in links:
+                    stmts[i] = f"[{s}]({links[s]})"
+
             rows.append(["Match qualifiers", ", ".join(stmts)])
         stmts = function.value_qualifiers
         stmts = [
@@ -227,6 +245,9 @@ class FunctionDescriber:
             for s in stmts
         ]
         if len(stmts) > 0:
+            for i, s in enumerate(stmts):
+                if markdown and links and s in links:
+                    stmts[i] = f"[{s}]({links[s]})"
             rows.append(["Value qualifiers", ", ".join(stmts)])
         if function.name_qualifier:
             rows.append(
--- gen_docs.py
+++ gen_docs.py
@@ -17,7 +17,38 @@ if __name__ == "__main__":
     #
     # make pages
     #
+
+    links = {
+        "Term": "https://github.com/csvpath/csvpath/blob/main/docs/matching.md#term",
+        "Variable": "https://github.com/csvpath/csvpath/blob/main/docs/matching.md#variable",
+        "Reference": "https://github.com/csvpath/csvpath/blob/main/docs/matching.md#reference",
+        "Header": "https://github.com/csvpath/csvpath/blob/main/docs/matching.md#header",
+        "Function": "https://github.com/csvpath/csvpath/blob/main/docs/matching.md#function",
+        "asbool": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#asbool",
+        "decrease": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#decrease",
+        "distinct": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#distinct",
+        "increase": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#increase",
+        "latch": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#latch",
+        "nocontrib": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#nocontrib",
+        "notnone": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#notnone",
+        "once": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#once",
+        "onchange": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#onchange",
+        "onmatch": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#onmatch",
+        "renew": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#renew",
+        "strict": "https://github.com/csvpath/csvpath/blob/main/docs/qualifiers.md#strict",
+        "str": "",
+        "int": "",
+        "float": "",
+        "date": "",
+        "datetime": "",
+        "None": "",
+        "''": "",
+        "bool": "",
+        "Any": "",
+    }
+
     names = list(FunctionFactory.MY_FUNCTIONS.keys())
+    names.sort()
     for _ in names:
         f = FunctionFactory.get_function(
             None, name=_, child=None, find_external_functions=False
@@ -25,7 +56,7 @@ if __name__ == "__main__":
         ps = io.StringIO()
         FunctionDescriber.CONST = MyConst
         with redirect_stdout(ps):
-            FunctionDescriber.describe(f, markdown=True)
+            FunctionDescriber.describe(f, markdown=True, links=links)
             docdir = os.path.join(".", "docs", "func_gen")
             if not os.path.exists(docdir):
                 os.makedirs(docdir)
@@ -37,6 +68,7 @@ if __name__ == "__main__":
     #
     index = "# Functions Index\n"
     index = f"{index}These function docs are generated from the function code. They are also available in the CsvPath Framework [CLI](https://www.csvpath.org) and [FlightPath Data](https://www.flightpathdata.com).\n"
+    index = f"{index}\n\n[[Back to overview](https://github.com/csvpath/csvpath/blob/main/docs/functions.md)]\n\n"
     docdir = os.path.join(".", "docs", "func_gen")
     for _ in names:
         index += f"* [{_}](https://github.com/csvpath/csvpath/blob/main/docs/func_gen/{_}.md)\n"
