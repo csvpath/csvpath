c524b447628a1e0eb2ceaeda41175851b08df0e3
David Kershaw <dk107dk@hotmail.com>
2026-03-03 12:02:20 -0500

    parquet and transfer tests

---
+++ tests/csvpaths/examples/csvpaths_examples_parquet/test_csvpaths_examples_parquet.py
@@ -0,0 +1,134 @@
+import unittest
+import tempfile
+import os
+import shutil
+import pyarrow.parquet as pq
+from csvpath import CsvPaths
+from csvpath.managers.paths.paths_manager import PathsManager
+from csvpath.util.file_readers import DataFileReader
+from csvpath.util.nos import Nos
+
+PATH = f"tests{os.sep}csvpaths{os.sep}test_resources{os.sep}named_paths{os.sep}parquet.csvpaths"
+PATH2 = f"tests{os.sep}csvpaths{os.sep}test_resources{os.sep}named_paths{os.sep}parquet2.csvpaths"
+
+
+class TestCsvPathsExamplesPathsParquet(unittest.TestCase):
+    def test_csvpaths_parquet_1(self) -> None:
+        paths = CsvPaths()
+        paths.config.add_to_config("errors", "csvpaths", "raise, collect, print")
+        paths.config.add_to_config("errors", "csvpath", "raise, collect, print")
+        #
+        # setup paths
+        #
+        paths.paths_manager.add_named_paths_from_file(
+            name="annual_units", file_path=PATH
+        )
+        paths.file_manager.add_named_file(
+            name="autos",
+            path=os.path.join(
+                "tests",
+                "csvpaths",
+                "test_resources",
+                "Automobiles_Annual_Imports_and_Exports_Port_Authority_of_NY.csv",
+            ),
+        )
+        ref = paths.collect_paths(filename="autos", pathsname="annual_units")
+        results = paths.results_manager.get_named_results(ref)
+        assert results
+        assert len(results) == 1
+        result = results[0]
+        rundir = result.run_dir
+        path = Nos(rundir).join("autos")
+        path = Nos(path).join("autos.parquet")
+        path = Nos(path)
+        assert path.exists()
+
+        #
+        # parquet doesn't like our backend urls so copy to a local temp
+        #
+        path = Nos(paths.run_metadata.run_home)
+        path = path.join("autos")
+        pname = Nos(path).join("autos.parquet")
+        with DataFileReader(path=pname, mode="rb") as src, tempfile.NamedTemporaryFile(
+            mode="wb"
+        ) as dst:
+            shutil.copyfileobj(src.source, dst)
+            dst.flush()
+            p = pq.ParquetFile(dst.name)
+            table = p.read(columns=["year", "type", "volume"])
+            assert table
+            chunked_array = table.column("volume")
+            assert len(chunked_array) == 2
+            assert int(chunked_array[1]) == 302441
+
+    def test_csvpaths_parquet_2(self) -> None:
+        paths = CsvPaths()
+        paths.config.add_to_config("errors", "csvpaths", "raise, collect, print")
+        paths.config.add_to_config("errors", "csvpath", "raise, collect, print")
+        #
+        # setup paths
+        #
+        paths.paths_manager.add_named_paths_from_file(
+            name="annual_units", file_path=PATH2
+        )
+        paths.file_manager.add_named_file(
+            name="autos",
+            path=os.path.join(
+                "tests",
+                "csvpaths",
+                "test_resources",
+                "Automobiles_Annual_Imports_and_Exports_Port_Authority_of_NY.csv",
+            ),
+        )
+        ref = paths.collect_paths(filename="autos", pathsname="annual_units")
+        results = paths.results_manager.get_named_results(ref)
+        assert results
+        assert len(results) == 1
+        result = results[0]
+        rundir = result.run_dir
+        path = Nos(rundir).join("autos")
+        path = Nos(path).join("autos.parquet")
+        path = Nos(path)
+        assert path.exists()
+
+        rundir = result.run_dir
+        path = Nos(rundir).join("autos")
+        path = Nos(path).join("years.parquet")
+        path = Nos(path)
+        assert path.exists()
+
+        #
+        # parquet doesn't like our backend urls so copy to a local temp
+        #
+        path = Nos(paths.run_metadata.run_home)
+        path = path.join("autos")
+        pname = Nos(path).join("autos.parquet")
+        with DataFileReader(path=pname, mode="rb") as src, tempfile.NamedTemporaryFile(
+            mode="wb"
+        ) as dst:
+            shutil.copyfileobj(src.source, dst)
+            dst.flush()
+            p = pq.ParquetFile(dst.name)
+            table = p.read(columns=["year", "type", "volume"])
+            assert table
+            chunked_array = table.column("volume")
+            assert len(chunked_array) == 32
+            assert int(chunked_array[1]) == 302441
+
+        #
+        # parquet doesn't like our backend urls so copy to a local temp
+        #
+        path = Nos(paths.run_metadata.run_home)
+        path = path.join("autos")
+        pname = Nos(path).join("years.parquet")
+        with DataFileReader(path=pname, mode="rb") as src, tempfile.NamedTemporaryFile(
+            mode="wb"
+        ) as dst:
+            shutil.copyfileobj(src.source, dst)
+            dst.flush()
+            p = pq.ParquetFile(dst.name)
+            table = p.read(columns=["year"])
+            assert table
+            chunked_array = table.column("year")
+            assert len(chunked_array) == 6
+            assert int(chunked_array[2]) == 2009
---
+++ tests/csvpaths/examples/csvpaths_examples_transfer_sftp/test_csvpaths_examples_transfer_sftp.py
@@ -0,0 +1,82 @@
+import unittest
+import tempfile
+import os
+import shutil
+import pyarrow.parquet as pq
+
+from csvpath import CsvPaths
+from csvpath.util.nos import Nos
+from csvpath.util.file_readers import DataFileReader
+
+CSV = os.path.join(
+    "tests",
+    "csvpaths",
+    "examples",
+    "csvpaths_examples_transfer_sftp",
+    "csvs",
+    "March-2024.csv",
+)
+PATH = os.path.join(
+    "tests",
+    "csvpaths",
+    "examples",
+    "csvpaths_examples_transfer_sftp",
+    "csvpaths",
+    "transfer.csvpath",
+)
+
+
+class TestCsvPathsExamplesTransferSftp(unittest.TestCase):
+    def test_transfer_parquet_1(self):
+        paths = CsvPaths()
+        paths.add_to_config("errors", "csvpaths", "raise, collect, print")
+        paths.add_to_config("errors", "csvpath", "raise, collect, print")
+        paths.config.set(section="sftp", name="server", value="192.168.1.152")
+        paths.config.set(section="sftp", name="port", value="2022")
+        paths.config.set(section="sftp", name="username", value="python")
+        paths.config.set(section="sftp", name="password", value="hangzhou")
+        #
+        # clear and add files
+        #
+        if paths.file_manager.has_named_file("transfer"):
+            paths.file_manager.remove_named_file("transfer")
+        assert not paths.file_manager.has_named_file("transfer")
+        paths.file_manager.add_named_file(name="transfer", path=CSV)
+        #
+        # clear and add paths
+        #
+        if paths.paths_manager.has_named_paths("transfer"):
+            paths.paths_manager.remove_named_paths("transfer")
+        assert not paths.paths_manager.has_named_paths("transfer")
+        paths.paths_manager.add_named_paths(name="transfer", from_file=PATH)
+        #
+        # run paths vs files
+        #
+        paths.collect_paths(filename="transfer", pathsname="transfer")
+        #
+        # create local check path and check that parquet file exists
+        #
+        ppath = Nos(paths.run_metadata.run_home).join("output")
+        ppath = Nos(ppath).join("stores.parquet")
+        assert Nos(ppath).exists()
+        #
+        # copy parquet file to temp local and check contents
+        #
+        path = Nos(paths.run_metadata.run_home).join("output")
+        pname = Nos(path).join("stores.parquet")
+        with DataFileReader(path=pname, mode="rb") as src, tempfile.NamedTemporaryFile(
+            mode="wb"
+        ) as dst:
+            shutil.copyfileobj(src.source, dst)
+            dst.flush()
+            p = pq.ParquetFile(dst.name)
+            table = p.read(columns=["id", "day"])
+            assert table
+            chunked_array = table.column("day")
+            assert len(chunked_array) == 3
+            assert chunked_array[2].as_py().day == 21
+        #
+        # check that transfer happened
+        #
+        tpath = "sftp://stores.parquet"
+        assert Nos(tpath).exists()
